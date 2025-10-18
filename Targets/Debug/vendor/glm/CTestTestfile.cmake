895a5dd801507b6e83b210e34515b2b8b8c682b9
Nesmy Point du Jour <pnesmy@gmail.com>
2025-10-18 08:02:57 -0400

    remove targets

--- Targets/Debug/vendor/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nephilim/Projects/Bounce/vendor/glm
-# Build directory: /home/nephilim/Projects/Bounce/Targets/Debug/vendor/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- Targets/Debug/vendor/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nephilim/Projects/Bounce/vendor/glm/glm
-# Build directory: /home/nephilim/Projects/Bounce/Targets/Debug/vendor/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
